727a2f99667bd0e8b640225c29904a685eb7bf89
An Nguyen <an-1258@outlook.com>
2023-05-08 10:24:42 +0400

    Core - Update ICrudService interface

--- packages/core/src/services/crud-service.interface.ts
+++ packages/core/src/services/crud-service.interface.ts
@@ -3,5 +3,5 @@ import { Observable } from 'rxjs'
 export interface ICrudService<TId, TEntity> {
   create(entity: TEntity): Observable<TEntity>
   update(id: TId, entity: TEntity): Observable<TEntity>
-  delete(id: TId): Observable<void>
+  delete(id: TId): Observable<unknown>
 }
