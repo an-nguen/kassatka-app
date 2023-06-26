656e7f06f9f89dfb8522ac9765641e16eff99dc0
An Nguyen <an-1258@outlook.com>
2023-06-26 21:15:19 +0400

    Client - Update App module and component

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { SharedModule } from './shared/shared.module'
-import { MainLayoutModule } from './core/components/main-layout/main-layout.module'
-import { DashboardModule } from './features/dashboard/dashboard.module'
-import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
-import { ServerInternalErrorComponent } from './core/components/server-internal-error/server-internal-error.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutModule } from './core/components/main-layout/main-layout.module';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { ServerInternalErrorComponent } from './core/components/server-internal-error/server-internal-error.component';
+import { DashboardModule } from './features/dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ServerInternalErrorComponent } from './core/components/server-internal-
     MainLayoutModule,
     SharedModule,
     DashboardModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- packages/client/src/app/features/document/services/document.service.ts
+++ packages/client/src/app/features/document/services/document.service.ts
@@ -1,20 +1,28 @@
-import { Injectable } from '@angular/core'
-import { IDocument, IDocumentService, IPage } from '@kassatka/core'
-import { Observable } from 'rxjs'
-import { environment } from 'src/environments/environment'
-import { HttpClient, HttpParams } from '@angular/common/http'
-import { formatISO } from 'date-fns/fp'
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IDocument, IDocumentService, IPage } from '@kassatka/core';
+import { formatISO } from 'date-fns/fp';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'any',
 })
 export class DocumentService implements IDocumentService {
-  private readonly _documentPath: string
-  private readonly _httpClient: HttpClient
+  private readonly _documentPath: string;
+  private readonly _httpClient: HttpClient;
 
   constructor(private httpClient: HttpClient) {
-    this._httpClient = httpClient
-    this._documentPath = environment.BACKEND_DOCUMENT_URL_PATH
+    this._httpClient = httpClient;
+    this._documentPath = environment.BACKEND_DOCUMENT_URL_PATH;
+  }
+
+  public getPage(
+    page: number,
+    pageSize: number,
+    sortBy?: string[] | undefined
+  ): Observable<IPage<IDocument>> {
+    throw new Error('Method not implemented.');
   }
 
   public getRefundsRange(
@@ -28,7 +36,7 @@ export class DocumentService implements IDocumentService {
           .set('startDate', formatISO(startDate))
           .set('endDate', formatISO(endDate)),
       }
-    )
+    );
   }
 
   public getSalesRange(
@@ -42,6 +50,6 @@ export class DocumentService implements IDocumentService {
           .set('startDate', formatISO(startDate))
           .set('endDate', formatISO(endDate)),
       }
-    )
+    );
   }
 }
