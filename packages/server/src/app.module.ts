2d0e386aec2f15276e3ce9180aebfb000276d4fd
An Nguyen <an-1258@outlook.com>
2023-06-26 14:29:57 +0400

    Server - Auto-validation

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,18 +1,21 @@
+import { HttpModule } from '@nestjs/axios'
 import {
-  ConsoleLogger, MiddlewareConsumer,
+  ConsoleLogger,
+  MiddlewareConsumer,
   Module,
   NestModule,
 } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from 'app.controller'
 import { AppService } from 'app.service'
-import { ProductsController } from 'modules/products/products.controller'
-import { ProductsService } from 'modules/products/products.service'
-import { CategoriesController } from 'modules/categories/categories.controller'
-import { CategoriesService } from 'modules/categories/categories.service'
-import { KassatkaApiService } from 'shared/services/kassatka-api/kassatka-api.service'
-import { HttpModule } from '@nestjs/axios'
 import { configuration } from 'core/configuration'
+import { CategoryController } from 'modules/category/category.controller'
+import { CategoryService } from 'modules/category/category.service'
+import { DocumentController } from 'modules/document/document.controller'
+import { DocumentService } from 'modules/document/document.service'
+import { ProductController } from 'modules/products/product.controller'
+import { ProductService } from 'modules/products/product.service'
+import { KassatkaApiService } from 'shared/services/kassatka-api/kassatka-api.service'
 
 @Module({
   imports: [
@@ -21,17 +24,22 @@ import { configuration } from 'core/configuration'
     }),
     HttpModule,
   ],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [
+    AppController,
+    ProductController,
+    CategoryController,
+    DocumentController,
+  ],
   providers: [
     AppService,
-    ProductsService,
-    CategoriesService,
+    ProductService,
+    CategoryService,
     ConsoleLogger,
     KassatkaApiService,
+    DocumentService,
   ],
   exports: [ConsoleLogger],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-  }
+  configure(consumer: MiddlewareConsumer): any {}
 }
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,31 +1,33 @@
-import { ConfigService } from '@nestjs/config'
-import { NestFactory } from '@nestjs/core'
+import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify'
-import { ConsoleLogger } from '@nestjs/common'
-import { AppModule } from 'app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { CONST } from 'core/constants'
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from 'app.module';
+import { CONST } from 'core/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
-  )
-  app.enableCors()
-  app.useLogger(app.get(ConsoleLogger))
-  const configService = app.get<ConfigService>(ConfigService)
+  );
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.useLogger(app.get(ConsoleLogger));
+  const configService = app.get<ConfigService>(ConfigService);
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Kassatka Local API')
     .setDescription('Internal API that works with Kassatka')
     .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, swaggerConfig)
-  SwaggerModule.setup('api', app, document)
-  const port = configService.get<number>(CONST.config.portPath) || 8080
-  await app.listen(port)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  const port = configService.get<number>(CONST.config.portPath) || 8080;
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(port);
 }
 
-bootstrap()
+bootstrap();
